9402e38594679638b7ed0a4d8875309d4c5a6905
ken yu <kenniyu@gmail.com>
2012-10-05 13:06:45 -0700

    modified to alert when im djing

--- tt_alert.js
+++ tt_alert.js
@@ -1,11 +1,14 @@
 (function() {
   var $body = $('body'),
-      $audioDiv = $('<audio id="myPing" src="http://jjc.changsclan.net/ken/tri-tone.mp3" preload="auto"></audio>'),
+      $audioDiv = $('<audio id="myPing" src="http://jjc.changsclan.net/ken/tri-tone.mp3" preload="auto"></audio><audio id="djTime" src="http://jjc.changsclan.net/ken/mail_mother-fuka-ringtone.mp3" preload="auto"></audio>'),
       command,
       userId,
       chatText,
       audioEl,
-      msgCount = 1;
+      currentDj,
+      msgCount = 1,
+      MY_USER_ID = '4e0646ef4fe7d05e0f00e44a',
+      BOT_USER_ID = '4faf2ceceb35c11f31000439';
 
   $body.append($audioDiv);
 
@@ -16,10 +19,11 @@
       $(item).click();
     }
   });
-  $('#privateChatIcon').click();
+  setTimeout(function() { $('#privateChatIcon').click(); }, 1000);
 
   turntable.addEventListener('message', function(m) {
     command = m['command'];
+    console.log(m)
     if (command === 'pmmed') {
       userId = m['senderid'];
       if (userId === '4faf2ceceb35c11f31000439') {
@@ -27,6 +31,12 @@
         audioEl = document.getElementById('myPing');
         audioEl.play();
       }
+    } else if (command === 'newsong') {
+			currentDj = m['room']['metadata']['current_dj'];
+			if (currentDj === '4e0646ef4fe7d05e0f00e44a') {
+				audioEl = document.getElementById('djTime');
+        audioEl.play();
+			}
     }
   });
 
